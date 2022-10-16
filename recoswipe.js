c978e5c033395a30428be195150e6908da31d307
Bill <bill@silvernovus.com>
2022-10-16 10:41:05 -1000

    Swiper additions

--- recoswipe.js
+++ recoswipe.js
@@ -7,20 +7,28 @@ const swiper = new Swiper('.swiper', {
    direction: 'horizontal',
    loop: true,
  
+   effect: "flip",
+
+   autoplay: {
+    delay: 10,
+    disableOnInteraction: true,
+  },
+
    // If we need pagination
    pagination: {
-     el: '.swiper-pagination',
+   /*  el: '.swiper-pagination', */
    },
  
    // Navigation arrows
    navigation: {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
+  
    },
  
    // And if we need scrollbar
    scrollbar: {
-     el: '.swiper-scrollbar',
+ /*    el: '.swiper-scrollbar',  */
    },
  });
 
